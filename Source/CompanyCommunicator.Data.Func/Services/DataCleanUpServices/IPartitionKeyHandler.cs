92929ac81cc125b3df136764c2da31eae1a6cd63
v-jaygupta <115773960+v-jaygupta@users.noreply.github.com>
2023-03-28 16:48:17 +0530

    Update IPartitionKeyHandler.cs
    Bug fix for Notification Data

--- Source/CompanyCommunicator.Data.Func/Services/DataCleanUpServices/IPartitionKeyHandler.cs
+++ Source/CompanyCommunicator.Data.Func/Services/DataCleanUpServices/IPartitionKeyHandler.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="IPartitionKeyHandler.cs" company="Microsoft">
+// <copyright file="IPartitionKeyHandler.cs" company="Microsoft">
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 // </copyright>
@@ -20,6 +20,15 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Data.Func.Services.DataCleanU
         /// <param name="purgeRecordsOlderThanDaysEndDate">End date for records deletion.</param>
         /// <returns>Table Query based on the start and end date.</returns>
         TableQuery GetTableQuery(int purgeRecordsOlderThanDaysStartDate, int purgeRecordsOlderThanDaysEndDate);
+
+
+        /// <summary>
+        /// Used to create the table query based on start, end date and partition key for records deletion.
+        /// </summary>
+        /// <param name="purgeRecordsOlderThanDaysStartDate">Start Date for records deletion.</param>
+        /// <param name="purgeRecordsOlderThanDaysEndDate">End date for records deletion.</param>
+        /// <returns>Table Query based on the start and end date.</returns>
+        TableQuery GetNotificationDataTableQuery(int purgeRecordsOlderThanDaysStartDate, int purgeRecordsOlderThanDaysEndDate);
 
         /// <summary>
         /// Breaks up a result page into partitions grouped by PartitionKey.
