abfd259077140fb41c5649297f6a1223963b395f
v-jaygupta <115773960+v-jaygupta@users.noreply.github.com>
2023-06-21 00:38:30 +0530

    Update scheduledMessages.tsx

--- Source/CompanyCommunicator/ClientApp/src/components/ScheduledMessages/scheduledMessages.tsx
+++ Source/CompanyCommunicator/ClientApp/src/components/ScheduledMessages/scheduledMessages.tsx
@@ -7,12 +7,14 @@ import { Spinner } from '@fluentui/react-components';
 import { GetScheduledMessagesAction } from '../../actions';
 import { RootState, useAppDispatch, useAppSelector } from '../../store';
 import { ScheduledMessageDetail } from './scheduledMessageDetail';
+import * as CustomHooks from '../../useInterval';
 
 export const ScheduledMessages = () => {
   const { t } = useTranslation();
   const scheduledMessages = useAppSelector((state: RootState) => state.messages).scheduledMessages.payload;
   const loader = useAppSelector((state: RootState) => state.messages).isScheduledMessagesFetchOn.payload;
   const dispatch = useAppDispatch();
+  const delay = 60000;
 
   React.useEffect(() => {
     if (scheduledMessages && scheduledMessages.length === 0) {
@@ -20,6 +22,10 @@ export const ScheduledMessages = () => {
     }
   }, [scheduledMessages]);
 
+ CustomHooks.useInterval(() => {
+    GetScheduledMessagesAction(dispatch);
+  }, delay);
+  
   return (
     <>
       {loader && <Spinner labelPosition='below' label='Fetching...' />}
